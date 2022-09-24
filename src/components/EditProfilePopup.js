b5b44ea23aceeb78a9ee10b08a8d329cab1e98cc
Alexandr Idgenov <cop2100@yandex.ru>
2022-09-24 23:06:04 +0700

    corr editProfilePopup component

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, useContext } from "react";
-import PopupEditProfile from "./PopupEditProfile";
 import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
@@ -46,13 +45,35 @@ function EditProfilePopup({isOpen, onClose, onUpdateUser}) {
         onClose={onClose}
         onSubmit={handleSubmit}
       >
-      <PopupEditProfile
-        name={name}
-        description={description}
-        onChangeName={handleChangeName}
-        onchangeDescription={handleChangeDescription}
-      />
-      </PopupWithForm>
+      <>
+      <label className="popup__fieldset">
+        <input type="text"
+        className="popup__form-input popup__form-input_type_name-on"
+        id="profile-name"
+        placeholder="Ваше имя?"
+        name="name"
+        required
+        minLength="2"
+        maxLength="40"
+        value={name}
+        onChange={handleChangeName} />
+        <span className="popup__form-input-error profile-name-error"></span>
+      </label>
+      <label className="popup__fieldset">
+        <input type="text"
+        className="popup__form-input popup__form-input_type_description-on"
+        id="profile-description"
+        placeholder="Чем Вы занимаетесь?"
+        name="about"
+        required
+        minLength="2"
+        maxLength="200"
+        value={description}
+        onChange={handleChangeDescription} />
+        <span className="popup__form-input-error profile-description-error"></span>
+      </label>
+      </>
+    </PopupWithForm>
   );
 }
 
